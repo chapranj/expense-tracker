e2cd2a60c1ad71914fd05a903102d6e94be10353
Pranjal Chauhan <pranjalc@evertz.com>
2024-01-31 21:45:41 -0500

    First commit, made the initial components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import { useReducer } from 'react';
 import './App.css';
+import { AddTransaction } from './components/AddTransaction';
+import { Balance } from './components/Balance';
+import { Header } from './components/Header';
+import { IncomeExpress } from './components/IncomeExpress';
+import { TransactionList } from './components/TransactionList';
 
 function App() {
+
+  const initialState = {
+    count:0,
+    twocount: 2
+  }
+
+  const counterEditor = (state, action)=>{
+    switch(action.type){
+      case '+':
+        return {count: state.count + 1}
+
+      case '-':
+        return {count: state.count - 1}
+    }
+
+  }
+
+  const[state, dispatch] = useReducer(counterEditor, initialState);
+
+  const increment =
+  ()=>{
+    dispatch({type : '+'})
+  }
+
+  const decrement =
+  ()=>{
+    dispatch({type : '-'})
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <span>
+        {state.count}
+        {state.twocount}
+      </span>
+
+      <button onClick={increment} >+</button>
+      <button onClick={decrement} >-</button>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      {/* <Header></Header>
+      <div className='container'>
+        <Balance></Balance>
+        <IncomeExpress></IncomeExpress>
+        <TransactionList></TransactionList>
+        <AddTransaction></AddTransaction>
+      </div> */}
     </div>
   );
 }
 
 export default App;
+
